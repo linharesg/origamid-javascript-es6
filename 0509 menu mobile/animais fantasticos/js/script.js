aa97c24738d77d214fa0aadc7115b7c90dae4006
Gabriel Linhares <linhares.gs96@gmail.com>
2024-11-30 15:56:05 -0300

    0509 menu mobile: initial commit

---
+++ 0509 menu mobile/animais fantasticos/js/script.js
@@ -0,0 +1,15 @@
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initScrollAnimate from "./modules/animacao-scroll.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/init-tab-nav.js";
+import initModal from './modules/modal.js'
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+initSmoothScroll();
+initScrollAnimate();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
