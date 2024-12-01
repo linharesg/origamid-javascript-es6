697afe79523183ae86077c76ebcc1cb527d64f8b
Gabriel Linhares <linhares.gs96@gmail.com>
2024-12-01 11:12:26 -0300

    0511 date object: initial commit

---
+++ 0511 date object/animais fantasticos/js/script.js
@@ -0,0 +1,19 @@
+import initSmoothScroll from "./modules/scroll-suave.js";
+import initScrollAnimate from "./modules/animacao-scroll.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/init-tab-nav.js";
+import initModal from './modules/modal.js'
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+
+initSmoothScroll();
+initScrollAnimate();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
