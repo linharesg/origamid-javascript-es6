bb21dec40c601c3e52243cec184891d71d1d86f2
Gabriel Linhares <linhares.gs96@gmail.com>
2024-12-11 20:53:45 -0300

    initial commit

---
+++ 0608 animais fetch/animais fantasticos/js/modules/animacao-scroll.js
@@ -0,0 +1,20 @@
+export default function initScrollAnimate() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if (sections.length) {
+    const windowFraction = window.innerHeight * 0.6;
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowFraction < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else if(section.classList.contains('ativo')){
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animateScroll();
+    window.addEventListener("scroll", animateScroll);
+  }
+}
