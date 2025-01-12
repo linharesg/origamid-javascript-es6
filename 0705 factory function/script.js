972cb56b1701671bd471f03e326dfb502f938f1c
Gabriel Linhares <linhares.gs96@gmail.com>
2025-01-12 17:28:32 -0300

    0705 factory function

---
+++ 0705 factory function/script.js
@@ -0,0 +1,109 @@
+function createButton(text) {
+  function element() {
+    const buttonElement = document.createElement('button');
+    buttonElement.innerText = text;
+    return buttonElement;
+  }
+  return {
+    element: element,
+    text: text,
+  }
+}
+
+const comprarBtn = createButton('Comprar');
+
+function criarPessoa(nome, sobrenome) {
+  const nomeCompleto = `${nome} ${sobrenome}`;
+
+  function andar() {
+    return `${nomeCompleto} andou`;
+  }
+  function nadar() {
+    return `${nomeCompleto} nadou`;
+  }
+  return {
+    nome,
+    sobrenome,
+    andar,
+    nadar,
+  }
+}
+
+const designer = criarPessoa('Gabriel', 'Rafael');
+
+function criarPessoa(nome, sobrenome) {
+  const nomeCompleto = `${nome} ${sobrenome}`;
+  function andar() {
+    return `${nomeCompleto} andou`;
+  }
+  return Object.freeze({
+    nome,
+    sobrenome,
+    andar,
+  });
+}
+
+const designer1 = criarPessoa('Gabriel', 'Rafael');
+
+function Pessoa(nome) {
+  if (!(this instanceof Pessoa)) // ou (!new.target)
+    return new Pessoa(nome);
+  this.nome = nome;
+}
+
+Pessoa.prototype.andar = function() {
+  return `${this.nome} andou`;
+}
+
+const designer2 = Pessoa('Gabriel');
+
+function $$(selectedElements) {
+  const elements = document.querySelectorAll(selectedElements);
+
+  function on(onEvent, callback) {
+    elements.forEach(element => {
+      element.addEventListener(onEvent, callback);
+    });
+    return this; // retornar this irá funcionar da mesma forma
+  }
+
+  function hide() {
+    elements.forEach(element => {
+      element.style.display = 'none';
+    });
+    return this;
+  }
+
+  function show() {
+    elements.forEach(element => {
+      element.style.display = 'initial';
+    });
+    return this;
+  }
+
+  function addClass(className) {
+    elements.forEach(element => {
+      element.classList.add(className);
+    });
+    return this;
+  }
+
+  function removeClass(className) {
+    elements.forEach(element => {
+      element.classList.add(className);
+    });
+    return this;
+  }
+  
+  return Object.freeze({
+    elements,
+    on,
+    hide,
+    show,
+    addClass,
+    removeClass,
+  });
+}
+
+const buttons = $$('button');
+buttons.hide().show().addClass('ativo').removeClass('ativo');
